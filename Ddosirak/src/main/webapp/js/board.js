34853bcfda2f086b2095cb929f3a710b496af616
admin <admin@DESKTOP-D80DI21>
2024-05-28 15:06:45 +0900

    게시판 기능 15:04

--- Ddosirak/src/main/webapp/js/board.js
+++ Ddosirak/src/main/webapp/js/board.js
@@ -12,6 +12,7 @@ document.querySelector('#modBtn').addEventListener('click', function() {
 document.querySelector('#delBtn').addEventListener('click', function() {
 	if(confirm("삭제하시겠습니까?")== true){
 		document.forms.myFrm.action = "deleteBoard.do";
+//		alert('삭제되었습니다'); //삭제화면 호출
 		alert('삭제되었습니다'); //삭제화면 호출
 	}
 	
